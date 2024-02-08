47a16a0ef20b66efd526fefdd8a9c24c39ef4afa
Glen-SZH <haogen00@gmail.com>
2024-02-09 02:17:54 +0800

    added comments

---
+++ core/src/com/mygdx/game/managers/PlayerControlManager.java
@@ -0,0 +1,18 @@
+package com.mygdx.game.managers;
+
+public class PlayerControlManager extends IOManager {
+
+//not done yet ~
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    //
+
+}
