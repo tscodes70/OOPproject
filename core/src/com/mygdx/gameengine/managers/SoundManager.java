ed4bc789190abc67027663ae41b5ad68b6299fd5
Glen-SZH <haogen00@gmail.com>
2024-03-06 11:30:02 +0800

    Create SoundManager.java

---
+++ core/src/com/mygdx/gameengine/managers/SoundManager.java
@@ -0,0 +1,22 @@
+package com.mygdx.gameengine.managers;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Sound;
+
+public class SoundManager {
+
+    //This method helps to use sound anywhere in the engine just use the following format...
+    //can add more logic if needed, or add stuff like lists or something though i don't think needed here yet
+    //use soundManager.play(put/file/path/here/yes/yes.mp3) to have audio anywhere or even in if statements
+    public static void play(String filePath) {
+        Music music = Gdx.audio.newMusic(Gdx.files.internal(filePath));
+        music.play();
+    }
+
+    //just dipose
+    public void dispose(String filePath) {
+        Music music = Gdx.audio.newMusic(Gdx.files.internal(filePath));
+        music.dispose();
+    }
+}
