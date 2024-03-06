b9d4d673c6c2b07a6c9f5fadf0d880606c65cae5
Glen-SZH <haogen00@gmail.com>
2024-03-06 11:32:40 +0800

    Update SoundManager.java

--- core/src/com/mygdx/gameengine/managers/SoundManager.java
+++ core/src/com/mygdx/gameengine/managers/SoundManager.java
@@ -8,7 +8,7 @@ public class SoundManager {
 
     //This method helps to use sound anywhere in the engine just use the following format...
     //can add more logic if needed, or add stuff like lists or something though i don't think needed here yet
-    //use soundManager.play(put/file/path/here/yes/yes.mp3) to have audio anywhere or even in if statements
+    //use SoundManager.play(put/file/path/here/yes/yes.mp3) to have audio anywhere or even in if statements
     public static void play(String filePath) {
         Music music = Gdx.audio.newMusic(Gdx.files.internal(filePath));
         music.play();
